8f4deae6b9de5e4061599aefb07f642429704d6b
clarissa2307 <marthaleo07@gmail.com>
2021-02-26 18:18:54 -0600

    delete

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -79,5 +79,19 @@ class PostControllerTest extends TestCase
             $this->assertDatabaseHas('posts', ['title' => 'nuevo']);
     }
 
-   
+    public function test_delete()
+    {
+
+        $post = factory(Post::class)->create();
+        $user = factory(User::class)->create();
+ 
+        $response = $this->actingAs($user, 'api')->json('DELETE', "/api/posts/$post->id");
+
+        $response->assertSee(null)
+            ->assertStatus(204); //Sin contenido
+
+            $this->assertDatabaseMissing('posts', ['id' => $post -> $id]);
+    }
+
+    
 }
